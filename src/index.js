a9501c87c1956861076228a4755eba4f02d55203
unknown <mahendrauppari044@gmail.com>
2024-02-12 21:17:27 -0600

    cleanUp

--- src/index.js
+++ src/index.js
@@ -1,59 +1,35 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
-import NotFound from '../src/app/NotFound';
-import Resume from '../src/app/Resume';
-import Content from '../src/app/Content';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import NotFound from "../src/app/NotFound";
+import Resume from "../src/app/Resume";
+import Content from "../src/app/Content";
+import { createBrowserRouter } from "react-router-dom";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <NotFound />,
-    children:[{
-      
+    children: [
+      {
         path: "/Resume",
-        element: <Resume/>,
-    
+        element: <Resume />,
       },
       {
-      
         path: "Projects",
         element: <Content />,
-    
-      }]
-    
+      },
+    ],
   },
-  // {
-      
-  //   path: "/Resume",
-  //   element: <Resume/>,
-
-  // },
-  // {
-      
-  //   path: "/Projects",
-  //   element: <Content />,
-
-  // }
 ]);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-      {/* <RouterProvider router={router} ></RouterProvider> */}
-
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
